9ddbcbab9e3f909e9858638eaebfb02fa3d29095
Fred <delon.frederic.pro.job@gmail.com>
2017-01-16 14:06:07 +0100

    correction exo

--- index.php
+++ index.php
@@ -2,24 +2,47 @@
 
 class StrUtils {
 
-	private $str = ;
+	private $_str = "";
 
-	bold(){
-
+	function __construct($newString){
+		return $this->_str = $newString;
 	}
-	italic(){
 
-	}
-	underline(){
+	function bold(){
 
-	}
-	capitalize(){
-
-	}
-	uglify(){
+		$this->_str = "<b>".$this->_str."</b>";
+		return $this->_str;
 		
 
 	}
+	function italic(){
+		$this->_str = "<i>".$this->_str."</i>";
+		return $this->_str;
+		
+	}
+	function underline(){
+		$this->_str = "<u>".$this->_str."</u>";
+		return $this->_str;
+	}
+
+	function capitalize(){
+		return strtoupper($this->_str);
+	}
+
+	function uglify(){
+		  $this-> bold();
+		  $this->italic();
+		  $this->underline();
+
+	}
+	function ToString(){
+		  return $this->_str;
+
+	}
 }
+
+$myStr = new StrUtils("php is cool !");
+$myStr->uglify();
+echo $myStr->ToString();
 
 ?>
